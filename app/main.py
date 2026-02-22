9b6f68256233b6e1c3b21cfa2d8c5d6fa294ad58
SPMSPI <kislicin.artur@gmail.com>
2026-02-22 15:01:52 +0200

    fixed annotation type

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> list:
+    def serve_cars(self, cars: list) -> float:
         income = 0.0
         for car in cars:
             if self.clean_power > car.clean_mark:
@@ -22,7 +22,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def calculate_washing_price(self, car: list) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         income = (car.comfort_class * (self.clean_power - car.clean_mark)
                   * (self.average_rating / self.distance_from_city_center))
         return income
@@ -31,7 +31,7 @@ class CarWashStation:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rate: float) -> float:
+    def rate_service(self, rate: float) -> None:
         result = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
         self.average_rating = round((result + rate) / self.count_of_ratings, 1)
